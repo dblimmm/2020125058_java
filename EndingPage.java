cd518987ba8e525d05921b92bf4dd8662904e88a
icw0207 <78637658+dblimmm@users.noreply.github.com>
2021-12-08 16:58:28 +0900

    Update EndingPage.java
    코드 주석 정리

--- EndingPage.java
+++ EndingPage.java
@@ -27,12 +27,14 @@ public class EndingPage extends AppCompatActivity
         result = findViewById(R.id.result);
         winner = findViewById(R.id.winner);
 
+        //Main Activity에서 보낸 정보를 받아옵니다
         Intent intent = getIntent();
         String pc1Candy = intent.getStringExtra("pc1Candy");
         String pc2Candy = intent.getStringExtra("pc2Candy");
-
+        //플레이어 두명의 캔디 갯수를 출력
         result.setText("PC1 has " + pc1Candy + "candy, PC2 has " + pc2Candy +"candy");
 
+        //캔디가 많은 사람을 승자로 지정하여 화면에 출력
         if(Integer.parseInt(pc1Candy) > Integer.parseInt(pc2Candy))
         {
             winner.setText("So winner is PC1!");
@@ -46,7 +48,7 @@ public class EndingPage extends AppCompatActivity
             winner.setText("DRAW ...");
         }
 
-        //버튼을 누르면 mainActivity로 이동
+        //Restart 버튼을 누르면 mainActivity로 이동
         btnRestart.setOnClickListener(new View.OnClickListener()
         {
             @Override
